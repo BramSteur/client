a1225d62fd605dba6a0b285925d03958951ee168
Bram Steur <220507@student.glu.nl>
2024-01-29 11:07:48 +0100

    start-up

--- src/App.js
+++ src/App.js
@@ -1,97 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, InputGroup, FormControl, Button, Row, Card} from 'react-bootstrap';
-import {useState, useEffect } from 'react';
- 
-const CLIENT_ID = "7276076c86434629923e163eb1ba9682";
-const CLIENT_SECRET = "b08e0d99a79c40d2b6884715feb73978";
+import Detail from './pages/Detail';
  
 function App() {
-  const [searchInput, setSearchInput] = useState("");
-  const [accessToken, setAccestoken] = useState("");
-  const [albums, setAlbums] = useState([]);
-
-  useEffect(() => {
-    // API Access Token
-    var authParameters = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-    }
-    fetch('https://accounts.spotify.com/api/token', authParameters)
-      .then(result => result.json())
-      .then(data => setAccestoken(data.access_token))
-  }, [])
  
-  //Search
-  async function search() {
-    if (!searchInput) {
-      console.log("Search input is empty. Nothing to search for.");
-      return;
-    }
- 
-    var searchParameters = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer '+ accessToken
-      }
-    }
-    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist' , searchParameters)
-      .then(response => response.json())
-      .then(data => { return data.artists.items[0].id })
- 
-    console.log("Artist ID is " + artistID);
- 
-    var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=NL&limit=50', searchParameters)
-     .then(response => response.json()) 
-     .then(data => {
-        console.log(data);
-        setAlbums(data.items);
-     });
- 
-  }
- console.log(albums);
   return (
-    <div className="App">
-      <Container>
-        <InputGroup className="mb-3" size="lg">
-        <FormControl
-          placeholder="Search for an artist"
-          type="input"
-          onKeyPress={event => {
-            if (event.key == "Enter") {
-              search();
-            }
-          }}
-          onChange={event => setSearchInput(event.target.value)}
-          />
-          <Button onClick={event => {console.log(search())}}>
-            Search
-          </Button>
-        </InputGroup>
-      </Container>
-      <Container>
-        <Row className="mx-2 row row-cols-4">
-        {albums.map( (album, i) => {
-          console.log(album);
-          return (
-              <Card>
-          <Card.Img src={album.images[0].url} />
-          <Card.Body>
-            <Card.Title>{album.name}</Card.Title>
-            <Button>CLICK</Button>
-          </Card.Body>
-        </Card>
-            )
-          })}
-
-        </Row>
-      </Container>
-    </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail" element={<Detail />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
  
---
+++ src/pages/Detail.js
@@ -0,0 +1,9 @@
+function Detail() {
+    return (
+        <>
+            <div>Hello</div>
+        </>
+    )
+}
+
+export default Detail
